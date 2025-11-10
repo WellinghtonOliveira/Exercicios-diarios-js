6c10a4346ec837df12427109a1438685cca94882
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-11-10 08:22:35 -0300

    Create 004.js

---
+++ Exercicios/004.js
@@ -0,0 +1,16 @@
+function getExtension(filename) {
+
+  return filename;
+}
+
+
+
+/*
+
+Dada uma string representando o nome de um arquivo, retorne a extensão do arquivo.
+
+A extensão é a parte do nome do arquivo que vem depois do último ponto ( .).
+Se o nome do arquivo não contiver um ponto ou terminar com um ponto, retorne "none".
+A extensão deve ser retornada tal como está, preservando maiúsculas e minúsculas.
+
+*/
