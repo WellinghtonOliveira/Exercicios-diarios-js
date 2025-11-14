444ae81a54fd5acfcd843a8f9f6f61f6d4e79785
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-11-14 08:17:21 -0300

    Create 008.js

---
+++ Exercicios/008.js
@@ -0,0 +1,20 @@
+function daysUntilWeekend(dateString) {
+
+  return dateString;
+}
+
+
+console.log(daysUntilWeekend("2025-11-14"))
+
+
+/*
+
+Dada uma data no formato "YYYY-MM-DD", retorne o número de dias restantes até o fim de semana.
+
+O fim de semana começa no sábado.
+Se a data fornecida for sábado ou domingo, retorne "It's the weekend!".
+Caso contrário, retorne "X days until the weekend.", onde Xé o número de dias até sábado.
+Se Xfor 1, use "day"(singular) em vez de "days"(plural).
+Certifique-se de que o cálculo ignore seu fuso horário local.
+
+*/
