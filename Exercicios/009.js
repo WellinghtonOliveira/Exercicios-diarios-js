fed224de037bd8c44243a1d69cdbe03f59f5aba3
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-11-24 08:21:16 -0300

    Create 009.js

---
+++ Exercicios/009.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+/*
+
+Dada uma string de mensagem e uma string de validação, determine se a mensagem é válida.
+
+Uma mensagem é válida se cada palavra na mensagem começar com a letra correspondente na sequência de validação, nessa ordem.
+As letras não diferenciam maiúsculas de minúsculas.
+As palavras na mensagem são separadas por espaços simples.
+
+*/
