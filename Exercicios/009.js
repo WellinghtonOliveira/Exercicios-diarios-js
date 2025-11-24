0a0a0dc9f52aefccd6a240c2a358c4db7b099a84
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-11-24 08:22:16 -0300

    Update 009.js

--- Exercicios/009.js
+++ Exercicios/009.js
@@ -1,6 +1,19 @@
 
 
+function isValidMessage(message, validator) {
+  const words = message.split(" ");
+  if (words.length !== validator.length) return false;
 
+  for (let i = 0; i < words.length; i++) {
+    if (words[i][0].toLowerCase() !== validator[i].toLowerCase()) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isValidMessage("hello world", "hw"))
 
 
 /*
